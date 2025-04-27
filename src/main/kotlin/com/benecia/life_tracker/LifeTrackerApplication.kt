9de9b84c96b6624565c26b319c348cd9435e9965
Donghyeok Lim <jh07050@gmail.com>
2025-04-27 17:05:31 +0900

    chore: initialize life-tracker project

---
+++ src/main/kotlin/com/benecia/life_tracker/LifeTrackerApplication.kt
@@ -0,0 +1,11 @@
+package com.benecia.life_tracker
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LifeTrackerApplication
+
+fun main(args: Array<String>) {
+	runApplication<LifeTrackerApplication>(*args)
+}
---
+++ src/test/kotlin/com/benecia/life_tracker/LifeTrackerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.benecia.life_tracker
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LifeTrackerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
