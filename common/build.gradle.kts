e58708e98849e2f0680d76e6f5351cde9340f30c
Donghyeok Lim <jh07050@gmail.com>
2025-04-28 17:29:37 +0900

    chore(common): Module 의존성 설정

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -3,5 +3,8 @@ plugins {
     kotlin("plugin.spring")
 }
 
+val springBootVersion: String by project
+
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web:$springBootVersion")
 }
