7684bc3700b37757b0ced8b135934766ce53f3b5
Donghyeok Lim <jh07050@gmail.com>
2025-04-28 13:16:32 +0900

    chore: 멀티 모듈 세팅

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,10 @@
 plugins {
-	kotlin("jvm") version "1.9.25"
-	kotlin("plugin.spring") version "1.9.25"
-	id("org.springframework.boot") version "3.4.5"
-	id("io.spring.dependency-management") version "1.1.7"
+	kotlin("jvm") version "1.9.25" apply false
+	kotlin("plugin.spring") version "1.9.25" apply false
+	id("org.springframework.boot") version "3.4.5" apply false
+	id("io.spring.dependency-management") version "1.1.7" apply false
+	id("java")
 }
-
-group = "com.benecia"
-version = "0.0.1-SNAPSHOT"
 
 java {
 	toolchain {
@@ -14,27 +12,34 @@ java {
 	}
 }
 
-repositories {
-	mavenCentral()
-}
+allprojects {
+	group = "com.benecia"
+	version = "0.0.1-SNAPSHOT"
 
-dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	runtimeOnly("org.postgresql:postgresql")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-}
-
-kotlin {
-	compilerOptions {
-		freeCompilerArgs.addAll("-Xjsr305=strict")
+	repositories {
+		mavenCentral()
 	}
 }
 
-tasks.withType<Test> {
-	useJUnitPlatform()
+subprojects {
+	apply(plugin = "org.jetbrains.kotlin.jvm")
+	apply(plugin = "org.jetbrains.kotlin.plugin.spring")
+	apply(plugin = "org.springframework.boot")
+	apply(plugin = "io.spring.dependency-management")
+
+	dependencies {
+		implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+		implementation("org.jetbrains.kotlin:kotlin-reflect")
+		implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+		testImplementation("org.springframework.boot:spring-boot-starter-test")
+	}
+
+	tasks.withType<Test> {
+		useJUnitPlatform()
+	}
+}
+
+// app 모듈에만 spring-boot 플러그인 적용
+project(":app") {
+	apply(plugin = "org.springframework.boot")
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,7 @@
 rootProject.name = "life-tracker"
+
+include("app")
+include("todo")
+include("accountbook")
+include("note")
+include("common")
