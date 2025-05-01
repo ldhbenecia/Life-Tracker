ebfa67e22f6bac185c2debfc24a5ba97312db2f2
Donghyeok Lim <jh07050@gmail.com>
2025-05-01 23:47:55 +0900

    feat: User error code 추가 작성

--- common/src/main/kotlin/com/benecia/lifetracker/common/exception/ErrorCode.kt
+++ common/src/main/kotlin/com/benecia/lifetracker/common/exception/ErrorCode.kt
@@ -2,5 +2,11 @@ package com.benecia.lifetracker.common.exception
 
 enum class ErrorCode(val code: Int, val message: String) {
 
-    INTERNAL_SERVER_ERROR(500, "Internal Server Error")
+    // Default
+    INTERNAL_SERVER_ERROR(500, "Internal Server Error"),
+
+    // User
+    USER_NOT_FOUND(404, "User not found"),
+    USER_ALREADY_EXISTS(409, "User already exists"),
+    INVALID_USER_INPUT(400, "Invalid user input")
 }
