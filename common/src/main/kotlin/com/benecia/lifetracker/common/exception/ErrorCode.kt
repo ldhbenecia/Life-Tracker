66ad46e89151540ad01204fb4cad6d358a5090eb
Donghyeok Lim <jh07050@gmail.com>
2025-05-02 18:06:29 +0900

    feat(todo): Todo error code 추가 작성

--- common/src/main/kotlin/com/benecia/lifetracker/common/exception/ErrorCode.kt
+++ common/src/main/kotlin/com/benecia/lifetracker/common/exception/ErrorCode.kt
@@ -8,5 +8,10 @@ enum class ErrorCode(val code: Int, val message: String) {
     // User
     USER_NOT_FOUND(404, "User not found"),
     USER_ALREADY_EXISTS(409, "User already exists"),
-    INVALID_USER_INPUT(400, "Invalid user input")
+    INVALID_USER_INPUT(400, "Invalid user input"),
+
+    // Todo
+    TODO_NOT_FOUND(404, "Todo not found"),
+    INVALID_TODO_INPUT(400, "Invalid todo input"),
+    UNAUTHORIZED_TODO_ACCESS(403, "Unauthorized access to todo item")
 }
