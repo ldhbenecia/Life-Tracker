06618015b2c06c965eea8425727cdf432633622e
Donghyeok Lim <jh07050@gmail.com>
2025-05-02 18:05:23 +0900

    chore(todo): spring boot dependency

--- todo/build.gradle.kts
+++ todo/build.gradle.kts
@@ -5,4 +5,6 @@ plugins {
 
 dependencies {
     implementation(project(":common"))
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-jdbc")
 }
