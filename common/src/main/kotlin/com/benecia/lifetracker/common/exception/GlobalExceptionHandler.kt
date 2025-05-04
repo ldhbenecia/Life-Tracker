12b3a995f27d75f3b53bc1f6ceeb61d23e3dee8f
Donghyeok Lim <jh07050@gmail.com>
2025-05-04 18:30:50 +0900

    feat: NotValidException Handler 등록

--- common/src/main/kotlin/com/benecia/lifetracker/common/exception/GlobalExceptionHandler.kt
+++ common/src/main/kotlin/com/benecia/lifetracker/common/exception/GlobalExceptionHandler.kt
@@ -2,8 +2,10 @@ package com.benecia.lifetracker.common.exception
 
 import com.benecia.lifetracker.common.response.ErrorResponse
 import org.springframework.http.HttpStatus
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException
 
 @RestControllerAdvice
 class GlobalExceptionHandler {
@@ -23,6 +25,19 @@ class GlobalExceptionHandler {
             status = e.errorCode.code,
             error = e.errorCode.name,
             message = mapOf("detail" to e.errorCode.message)
+        )
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    fun handleValidationException(e: MethodArgumentNotValidException): ErrorResponse {
+        val fieldErrors = e.bindingResult.fieldErrors.associate {
+            it.field to (it.defaultMessage ?: "Invalid value")
+        }
+
+        return ErrorResponse(
+            status = HttpStatus.BAD_REQUEST.value(),
+            error = "VALIDATION_ERROR",
+            message = fieldErrors
         )
     }
 }
