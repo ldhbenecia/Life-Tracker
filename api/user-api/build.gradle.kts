adde40620dca5eba1f481e8cfb9043b130b8adb7
Donghyeok Lim <jh07050@gmail.com>
2025-05-04 18:30:15 +0900

    chore: validatior dependency 등록

--- api/user-api/build.gradle.kts
+++ api/user-api/build.gradle.kts
@@ -7,4 +7,6 @@ dependencies {
     implementation(project(":common"))
     implementation(project(":core:user-core"))
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.hibernate.validator:hibernate-validator:8.0.1.Final")
+    implementation("org.glassfish:jakarta.el:4.0.2")
 }
