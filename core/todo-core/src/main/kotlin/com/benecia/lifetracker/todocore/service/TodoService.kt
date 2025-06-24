13643d8fdbfcfd2466eae1f668279f866464676d
Donghyeok Lim <jh07050@gmail.com>
2025-06-24 22:11:22 +0900

    chore: db 모듈 분리로 인한 코드 정리

--- core/todo-core/src/main/kotlin/com/benecia/lifetracker/todocore/service/TodoService.kt
+++ core/todo-core/src/main/kotlin/com/benecia/lifetracker/todocore/service/TodoService.kt
@@ -1,64 +1,7 @@
 package com.benecia.lifetracker.todocore.service
 
-import com.benecia.lifetracker.common.exception.CustomException
-import com.benecia.lifetracker.common.exception.ErrorCode
-import com.benecia.lifetracker.todocore.domain.Todo
-import com.benecia.lifetracker.todocore.repository.TodoRepository
-import com.benecia.lifetracker.usercore.service.UserValidationService
 import org.springframework.stereotype.Service
-import java.util.*
 
 @Service
-class TodoService(
-    private val todoRepository: TodoRepository,
-    private val userValidationService: UserValidationService,
-) {
-
-    fun createTodo(todo: Todo): Todo {
-        userValidationService.validateUserExists(todo.userId)
-        return todoRepository.save(todo)
-    }
-
-    fun getTodoById(id: UUID): Todo {
-        return todoRepository.findById(id) ?: throw CustomException(ErrorCode.TODO_NOT_FOUND)
-    }
-
-    fun getAllTodos(): List<Todo> {
-        return todoRepository.findAll()
-    }
-
-    fun getTodosByUserId(userId: UUID): List<Todo> {
-        userValidationService.validateUserExists(userId)
-        return todoRepository.findAllByUserId(userId)
-    }
-
-    fun updateTodo(todo: Todo): Todo {
-        val existing = todoRepository.findById(todo.id!!)
-            ?: throw CustomException(ErrorCode.TODO_NOT_FOUND)
-
-        userValidationService.validateUserExists(todo.userId)
-        if (existing.userId != todo.userId) {
-            throw CustomException(ErrorCode.UNAUTHORIZED_TODO_ACCESS)
-        }
-
-        existing.updateFrom(todo)
-        return todoRepository.update(existing)
-    }
-
-    fun toggleTodoDone(id: UUID): Todo {
-        return todoRepository.toggleDone(id) ?: throw CustomException(ErrorCode.TODO_NOT_FOUND)
-    }
-
-    fun deleteTodo(id: UUID): Boolean {
-        todoRepository.findById(id) ?: throw CustomException(ErrorCode.TODO_NOT_FOUND)
-        return todoRepository.deleteById(id)
-    }
-
-    fun getStatisticsByUser(userId: UUID): Pair<Int, Int> {
-        userValidationService.validateUserExists(userId)
-
-        val totalCount = todoRepository.countAllByUserId(userId)
-        val doneCount = todoRepository.countDoneByUserId(userId)
-        return Pair(totalCount, doneCount)
-    }
+class TodoService() {
 }
--- core/user-core/src/main/kotlin/com/benecia/lifetracker/usercore/service/UserService.kt
+++ core/user-core/src/main/kotlin/com/benecia/lifetracker/usercore/service/UserService.kt
@@ -1,53 +1,8 @@
 package com.benecia.lifetracker.usercore.service
 
-import com.benecia.lifetracker.common.exception.CustomException
-import com.benecia.lifetracker.common.exception.ErrorCode
-import com.benecia.lifetracker.usercore.domain.User
-import com.benecia.lifetracker.usercore.repository.UserRepository
 import org.springframework.stereotype.Service
-import java.util.*
 
 
 @Service
-class UserService(private val userRepository: UserRepository) {
-
-    fun createUser(user: User): User {
-        userRepository.findByUsername(user.username)?.let {
-            throw CustomException(ErrorCode.USER_ALREADY_EXISTS)
-        }
-        return userRepository.save(user)
-    }
-
-    fun getUserById(id: UUID): User {
-        return userRepository.findById(id)
-            ?: throw CustomException(ErrorCode.USER_NOT_FOUND)
-    }
-
-    fun getUserByUsername(username: String): User {
-        return userRepository.findByUsername(username)
-            ?: throw CustomException(ErrorCode.USER_NOT_FOUND)
-    }
-
-    fun getAllUsers(): List<User> {
-        return userRepository.findByAll()
-    }
-
-    fun updateUser(user: User): User {
-        val existing = userRepository.findById(user.id!!)
-            ?: throw CustomException(ErrorCode.USER_NOT_FOUND)
-
-        userRepository.findByUsername(user.username)?.let {
-            if (it.id != user.id) {
-                throw CustomException(ErrorCode.USER_ALREADY_EXISTS)
-            }
-        }
-
-        existing.updateInfo(user.username, user.email)
-        return userRepository.save(existing)
-    }
-
-    fun softDeleteUserById(id: UUID) {
-        userRepository.findById(id) ?: throw CustomException(ErrorCode.USER_NOT_FOUND)
-        userRepository.softDeleteById(id)
-    }
+class UserService() {
 }
