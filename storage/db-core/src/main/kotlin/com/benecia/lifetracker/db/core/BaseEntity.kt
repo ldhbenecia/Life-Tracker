ef5795ec5b502bfedb8495d401774e9042b3ce79
Donghyeok Lim <jh07050@gmail.com>
2025-06-28 20:55:41 +0900

    refactor: id 기본 설정 해제

--- storage/db-core/src/main/kotlin/com/benecia/lifetracker/db/core/BaseEntity.kt
+++ storage/db-core/src/main/kotlin/com/benecia/lifetracker/db/core/BaseEntity.kt
@@ -1,20 +1,12 @@
 package com.benecia.lifetracker.db.core
 
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
 import jakarta.persistence.MappedSuperclass
 import org.hibernate.annotations.CreationTimestamp
 import org.hibernate.annotations.UpdateTimestamp
-import java.time.LocalDateTime
 import java.time.ZonedDateTime
 
 @MappedSuperclass
-internal abstract class BaseEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: Long? = null
-
+abstract class BaseEntity {
     @CreationTimestamp
     val createdAt: ZonedDateTime? = null
 
