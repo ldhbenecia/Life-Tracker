658be9f607d2490328affd05e8773dda414a18b1
Donghyeok Lim <jh07050@gmail.com>
2025-06-30 21:37:46 +0900

    feat: Logger를 위한 의존성 부여

--- core/user-core/build.gradle.kts
+++ core/user-core/build.gradle.kts
@@ -5,5 +5,5 @@ plugins {
 
 dependencies {
     implementation(project(":common"))
-    compileOnly("org.springframework:spring-context")
+    implementation("org.springframework.boot:spring-boot-starter-web")
 }
