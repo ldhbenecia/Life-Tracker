d82d225f585dc29f9fad95247070e7bc9f56f54d
Donghyeok Lim <jh07050@gmail.com>
2025-07-03 18:56:20 +0900

    remove: 불필요 파일 제거
    복잡성을 늘릴 수 있는 단일예외처리를 제거합니다.
    사용하지 않는 폴더단을 제거합니다.

--- core/user-core/src/main/kotlin/com/benecia/lifetracker/user/exception/UnauthorizedUserException.kt
+++
@@ -1,8 +0,0 @@
-package com.benecia.lifetracker.user.exception
-
-import com.benecia.lifetracker.common.exception.CustomException
-
-class UnauthorizedUserException(
-    val accessorId: Long,
-    val resourceId: String
-) : CustomException(UserErrorCode.FORBIDDEN_USER_ACCESS)
--- core/user-core/src/main/kotlin/com/benecia/lifetracker/user/exception/UserNotFoundException.kt
+++
@@ -1,7 +0,0 @@
-package com.benecia.lifetracker.user.exception
-
-import com.benecia.lifetracker.common.exception.CustomException
-
-class UserNotFoundException(
-    userId: String
-) : CustomException(UserErrorCode.USER_NOT_FOUND)
