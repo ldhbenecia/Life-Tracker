1862fe1bb7b7f5212e3aa863ab887a70c4bf9453
Donghyeok Lim <jh07050@gmail.com>
2025-07-04 18:35:28 +0900

    remove: Todo 불필요 파일 제거

--- core/todo-core/src/main/kotlin/com/benecia/lifetracker/todocore/exception/TodoNotFoundException.kt
+++
@@ -1,7 +0,0 @@
-package com.benecia.lifetracker.todocore.exception
-
-import com.benecia.lifetracker.common.exception.CustomException
-
-class TodoNotFoundException(
-    val todoId: Long
-) : CustomException(TodoErrorCode.TODO_NOT_FOUND)
--- core/todo-core/src/main/kotlin/com/benecia/lifetracker/todocore/exception/UnauthorizedTodoAccessException.kt
+++
@@ -1,8 +0,0 @@
-package com.benecia.lifetracker.todocore.exception
-
-import com.benecia.lifetracker.common.exception.CustomException
-
-class UnauthorizedTodoAccessException(
-    val todoId: Long,
-    val currentUserId: String,
-) : CustomException(TodoErrorCode.UNAUTHORIZED_TODO_ACCESS)
