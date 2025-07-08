20e87e98d9a72e9959702c9a3e459a4962c65590
Donghyeok Lim <jh07050@gmail.com>
2025-07-08 17:52:23 +0900

    chore: test 모듈 상속 및 adoc 뼈대 작성

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -6,12 +6,13 @@ plugins {
 }
 
 dependencies {
-    implementation(project(":support:monitoring"))
     implementation(project(":common"))
-
+    implementation(project(":support:monitoring"))
     implementation(project(":core:user-core"))
     implementation(project(":core:todo-core"))
     implementation(project(":storage:db-core"))
+
+    testImplementation(project(":tests:api-docs"))
 
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-security")
