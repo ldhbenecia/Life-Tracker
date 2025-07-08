2232218e509051e764bdc313289eade372b7b34c
Donghyeok Lim <jh07050@gmail.com>
2025-07-08 14:52:15 +0900

    chore: add ktlint, asciidoctor, kapt

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,23 @@
 plugins {
 	kotlin("jvm")
+	kotlin("kapt")
 	kotlin("plugin.spring") apply false
-	kotlin("plugin.jpa") version "1.9.25" apply false
+	kotlin("plugin.jpa") apply false
 	id("org.springframework.boot") apply false
 	id("io.spring.dependency-management") apply false
+	id("org.asciidoctor.jvm.convert") apply false
+	id("org.jlleitschuh.gradle.ktlint") apply false
 }
 
 java {
 	toolchain {
-		languageVersion = JavaLanguageVersion.of(21)
+		languageVersion = JavaLanguageVersion.of("${property("javaVersion")}")
 	}
 }
 
-val projectGroup: String by project
-val applicationVersion: String by project
-
 allprojects {
-	group = projectGroup
-	version = applicationVersion
+	group = "${property("projectGroup")}"
+	version = "${property("applicationVersion")}"
 
 	repositories {
 		mavenCentral()
@@ -26,16 +26,21 @@ allprojects {
 
 subprojects {
 	apply(plugin = "org.jetbrains.kotlin.jvm")
+	apply(plugin = "org.jetbrains.kotlin.kapt")
 	apply(plugin = "org.jetbrains.kotlin.plugin.spring")
 	apply(plugin = "org.jetbrains.kotlin.plugin.jpa")
 	apply(plugin = "org.springframework.boot")
 	apply(plugin = "io.spring.dependency-management")
+	apply(plugin = "org.asciidoctor.jvm.convert")
+	apply(plugin = "org.jlleitschuh.gradle.ktlint")
 
 	dependencies {
 		implementation("org.jetbrains.kotlin:kotlin-reflect")
+		implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+		implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 		testImplementation("org.springframework.boot:spring-boot-starter-test")
-		testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-		testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+		annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+		kapt("org.springframework.boot:spring-boot-configuration-processor")
 	}
 
 	tasks.getByName("bootJar") {
